87e56c616c88ec1b7f8a33f714cc7612b08fa0e8
Lee Chae Rin <chaerin0916@gmail.com>
2025-11-10 22:04:29 +0900

    feat: 라우터 설정

--- vetpet_server/urls.py
+++ vetpet_server/urls.py
@@ -1,22 +1,33 @@
-"""
-URL configuration for vetpet_server project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+# ViewSet 임포트
+from user.views import UserViewSet
+from hospital.views import HospitalViewSet
+from category.views import CategoryViewSet
+from pet.views import PetViewSet
+from post.views import PostViewSet
+from review.views import ReviewViewSet
+from reservation.views import ReservationViewSet
+from comment.views import CommentViewSet
+from post_image.views import PostImageViewSet
+
+# Router 생성
+router = DefaultRouter()
+
+# ViewSet 등록
+router.register(r'users', UserViewSet)
+router.register(r'hospitals', HospitalViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'pets', PetViewSet)
+router.register(r'post', PostViewSet)
+router.register(r'reviews', ReviewViewSet)
+router.register(r'reservations', ReservationViewSet)
+router.register(r'comments', CommentViewSet)
+router.register(r'post-images', PostImageViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
